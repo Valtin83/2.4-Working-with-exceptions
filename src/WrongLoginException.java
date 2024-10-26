9e052c3404ec8366fb0b0a5893e10eee0012531f
Valentin <zhe-valentin@mail.ru>
2024-10-26 16:47:42 +0700

    ДЗ 2.4 Исключения

---
+++ src/WrongLoginException.java
@@ -0,0 +1,9 @@
+public class WrongLoginException extends Exception{
+
+    // Конструктор по умолчанию
+
+    public WrongLoginException(String message) {
+        super(message);
+    }
+}
+
---
+++ src/WrongPasswordException.java
@@ -0,0 +1,8 @@
+public class WrongPasswordException extends Exception{
+
+    // Конструктор по умолчанию
+
+    public WrongPasswordException(String message) {
+        super(message);
+    }
+}
